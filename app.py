16d802a1623bf1a6428c49f2eb3e4d5240948505
Adam Sommer <asommer70@gmail.com>
2016-12-17 14:23:22 -0500

    Importing weather icons. Setup config file in defaults.cfg.  Using Open Weather Maps to get area weather and forecast.  Created new route to return JSON from Open Weather Maps and added JavaScript to populate the weather div HTML and update every hour.

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, jsonify
+import ConfigParser
+import urllib, json
+
+config = ConfigParser.RawConfigParser()
+config.read('defaults.cfg')
+WEATHER_API_KEY = config.get('openweathermap', 'api_key')
+CITY_ID = config.get('openweathermap', 'city_id')
+WEATHER_URL = config.get('openweathermap', 'url')
+WEATHER_UNITS = config.get('openweathermap', 'units')
+API_PARAMS = "?id=" + CITY_ID + "&units=" + WEATHER_UNITS + "&appid=" + WEATHER_API_KEY
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  name = 'Beans...'
-  return render_template('index.html', name=name)
+    name = 'Beans...'
+    return render_template('index.html', name=name)
+
+
+
+
+@app.route('/api/weather')
+def weather():
+    response = urllib.urlopen(WEATHER_URL + "/weather" + API_PARAMS) 
+    data = json.loads(response.read())
+    return jsonify(data)
+
+@app.route('/api/forecast')
+def forecast():
+    response = urllib.urlopen(WEATHER_URL + "/forecast" + API_PARAMS)
+    data = json.loads(response.read())
+    return jsonify(data)
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,6 +1,7 @@
-//$('#clock').fitText(1.3);
-
-function update() {
+//
+// Get and display the clock.
+//
+function updateClock() {
   var clock = document.getElementById('clock');
   var date = moment().format('MMMM D YYYY');
   var time = moment().format('h:mm');
@@ -8,4 +9,45 @@ function update() {
   clock.innerHTML += '<div id="date">' + date + '</div>';
 }
 
-setInterval(update, 1000);
+setInterval(updateClock, 1000);
+
+//
+// Get the curent temp in Boone.
+//
+function updateWeather() {
+  axios.get('/api/weather')
+    .then(function (response) {
+      console.log(response);
+      var weather = document.getElementById('weather');
+      var html = '';
+
+      html += '<div><h2>Temperature in ' + response.data.name + '</h2>';
+      html += response.data.main.temp + '&deg; <br/><br/>';
+      html += 'High: ' + response.data.main.temp_max + '&deg; <br/>';
+      html += 'Low: ' + response.data.main.temp_min + '&deg; <br/>';
+      html += '</div><br/>';
+
+      html += '<div><h3>Conditions</h3>';
+      html += '<strong>Clouds:</strong> ' + response.data.clouds.all + '%<br/>';
+      html += '<strong>Description:</strong> ' + response.data.weather[0].main + '<br/>';
+      html += '<img class="weather-icon" src="/static/assets/img/' + response.data.weather[0].icon + '.svg" />';
+      html += '</div>';
+
+      html += '<div><h4>Sunrise</h4>';
+      html += moment.unix(response.data.sys.sunrise).format('h:mm:ss a');
+      html += '</div>';
+
+      html += '<div><h4>Sunset</h4>';
+      html += moment.unix(response.data.sys.sunset).format('h:mm:ss a');
+      html += '</div>';
+
+      weather.innerHTML = html;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
+// Update every hour.
+setInterval(updateWeather, 1000 * 60 * 60);
