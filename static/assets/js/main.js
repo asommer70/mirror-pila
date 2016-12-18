c71e8ff1bf7ca0cb4f4fb87c1898d1d62a8d1611
Adam Sommer <asommer70@gmail.com>
2016-12-18 18:18:05 -0500

    Added Handlebars and created a Handlebar template for displaying the 5 day Forecast information.

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -39,3 +39,73 @@ function updateWeather() {
 // Call updateWeather() once to populate the page onload, then update every hour.
 updateWeather();
 setInterval(updateWeather, 1000 * 60 * 60);
+
+//
+// Get the forecast for Boone.
+//
+function updateForecast() {
+  axios.get('/api/forecast')
+    .then(function (response) {
+      var list = adjustForecastData(response.data.list);
+      console.log('list:', list);
+      var source   = document.getElementById("forecast").innerHTML;
+      var template = Handlebars.compile(source);
+      document.getElementById('forecast-html').innerHTML = template({list: list});
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+//
+// Get data for each day and put it in it in a new object.
+// Returns an array of days with temp data and times.
+//
+function adjustForecastData(data) {
+  days = [];
+  for (var i = 0; i < 6; i++) {
+    var dayKey = moment().add(i, 'day').format('MDY');
+    var day = moment().add(i, 'day').format('M-D-Y');
+
+    days[dayKey] = {
+      day: day,
+      times: [],
+    };
+  }
+
+  data.forEach(function(item) {
+  //for (var i = 0; i < data.length; i++) {
+    //var item = data[i];
+    var date = moment.unix(item.dt);
+    var dateKey = date.format('MDY');
+    console.log('dateKey:', dateKey);
+    var time = date.utcOffset(new Date().getTimezoneOffset()).format('hh:mm:ss');
+  
+    var rain = 0; 
+    if (item.rain.hasOwnProperty('3h')) {
+      rain = Math.round(item.rain['3h'] * 100);
+    }
+    
+    var timeData = {
+      time: time,
+      temp_max: item.main.temp_max,
+      temp_min: item.main.temp_min,
+      rain: rain,
+      clouds: item.clouds.all,
+      condition: item.weather[0].description,
+      icon: item.weather[0].icon,
+    }
+
+    //console.log('days{}:', days[date.format('MDY')]);
+    days[dateKey].times.push(timeData);
+  })
+  //}
+
+  return days;
+}
+
+//adjustForecastData([]);
+
+// Call updateWeather() once to populate the page onload, then update every hour.
+updateForecast();
+//setInterval(updateForecast, 1000 * 60 * 60);
